5c48382a890b533c4ef7aa635cc7b812d2e0ecf9
Balaji <bbandaru@nlsinc.com>
2016-12-09 12:11:02 -0600

    *****

--- MavenFB/src/test/java/FirstPack/SendingMessages.java
+++ MavenFB/src/test/java/FirstPack/SendingMessages.java
@@ -1,19 +1,14 @@
 package FirstPack;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
 
-public class SendingMessage {
-
-	public static void main(String[] args) throws InterruptedException {
-
-
-		WebDriver driver=new FirefoxDriver();
+public class SendingMessages {
+  @Test
+  public void f3() throws InterruptedException {
+	  WebDriver driver=new FirefoxDriver();
 		
 		driver.get("http://www.facebook.com");
 		driver.manage().window().maximize();
@@ -21,14 +16,15 @@ public class SendingMessage {
 		
 		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("nlstest16@gmail.com");
 		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("netlogic123");
 		Thread.sleep(2000);
+		
 		driver.findElement(By.xpath(".//*[@id='u_0_l']")).click();
 		
-		System.out.println("Logged in Successfully");
+		
 		
 		driver.findElement(By.xpath(".//*[@id='navItem_217974574879787']/a/div/span")).click();
-		
 		Thread.sleep(2000);
 		
 		driver.findElement(By.xpath(".//*[@id='u_jsonp_2_j']/button[1]")).click();
@@ -37,7 +33,7 @@ public class SendingMessage {
 		//driver.findElement(By.xpath(".//*[@id='u_0_19']/button[1]"));
 		System.out.println("Opened Messages Icon");
 		
-		
-	}
-
+		driver.quit();
+  
+  }
 }
